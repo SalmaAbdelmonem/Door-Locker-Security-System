f66686d024194af2ebbc96128b824ff79a40973d
SalmaAbdelmonem <96205385+SalmaAbdelmonem@users.noreply.github.com>
2022-07-08 17:25:12 +0200

    Delete Final Project-Eclipse directory

--- Final Project-Eclipse/ControlEcu/Debug/dc_motor.d
+++
@@ -1,8 +0,0 @@
-dc_motor.o dc_motor.o: ../dc_motor.c ../dc_motor.h ../std_types.h \
-  ../gpio.h
-
-../dc_motor.h:
-
-../std_types.h:
-
-../gpio.h:
--- Final Project-Eclipse/ControlEcu/Debug/eeprom.d
+++
@@ -1,7 +0,0 @@
-eeprom.o eeprom.o: ../eeprom.c ../eeprom.h ../std_types.h ../twi.h
-
-../eeprom.h:
-
-../std_types.h:
-
-../twi.h:
